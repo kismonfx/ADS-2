e8eef9dcd5b63a1485e642ab2517235f56ac97b9
kismonfx <63048601+kismonfx@users.noreply.github.com>
2020-04-15 16:36:31 +0300

    Create alg.cpp

---
+++ alg.cpp
@@ -0,0 +1,62 @@
+int countPairs1(int *arr, int len, int value)
+{
+	int k = 0; 
+	for (int i = 0; i < len-1; i++)
+	{
+		for (int j =i+1; j < len; j++)
+		{
+			if (arr[i] + arr[j] == value)
+			k++;
+		}
+	}
+	return k;
+}
+int countPairs2(int *arr,int len,int value)
+{
+  int k = 0;
+  for (int i = 0; i < len-1; i++)
+  {
+    for (int j = len-1; j > i; j--)
+    {
+      if (arr[i] + arr[j] == value)
+      {
+         k++;
+      }
+    }
+  }
+  return k;
+}
+int countPairs3(int *arr, int len, int value)
+{
+	int k = 0;
+	for (int i = 0; i < len - 1; i++)
+	{
+		int left = i, right = len;
+		while (left < right-1)
+		{
+			int mid = (left + right) / 2;
+			if (arr[i] + arr[mid] == value)
+			{
+				k++;
+				int j = mid+1;
+				while (arr[i] + arr[j] == value && j <right)
+				{
+					k++;
+					j++;
+				}
+				j = mid - 1;
+				while (arr[i] + arr[j] == value && j>left)
+				{
+					k++;
+					j--;
+				}
+				break;
+			}
+			if (arr[i] + arr[mid] > value)
+				right = mid;
+			else
+				left = mid;
+		}
+	}
+	return k;
+}
